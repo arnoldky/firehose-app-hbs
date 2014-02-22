29c9d436b95fdc139885fc14091128d27b180936
Arnold <achan@mba2015.hbs.edu>
2014-02-22 19:01:11 +0000

    my first commit for the hbs app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirehouseAppHbs2::Application.config.secret_key_base = '45af3a718576f8d31d58eaab9c50e5819fde116ab8e7f5afd5bcabb41331e884349a689b03d718f79816beb56574e957ca7bb597272c436818cc93e278f8ccf1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirehouseAppHbs2::Application.config.session_store :cookie_store, key: '_firehouse-app-hbs2_session'
